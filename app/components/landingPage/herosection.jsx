cca5d25771eb23a0af300473150b2954533bae1b
jieffrey <kyooon08@gmail.com>
2025-10-04 07:23:22 +0700

    push hero section

--- app/components/landingPage/herosection.jsx
+++ app/components/landingPage/herosection.jsx
@@ -2,8 +2,24 @@ import Image from "next/image"
 
 export default function Hero() {
     return(
-        <div className="">
-            
+        <div className="w-full min-h-[500px] flex items-center justify-between px-12 bg-gradient-to-r from-purple-100 to-blue-100">
+            {/* left section */}
+            <div className="max-w-lg">
+                <h1 className="text-5xl font-extrabold text-gray-900 mb-4">
+                    Unieash your Creativity!
+                </h1>
+                <p className="text-gray-600 mb-6">
+                    Turn the smallest beads into wonders that shine brighter than you ever imagined.
+                </p>
+                <div className="flex gap-4">
+                    <button className="px-6 py-3 bg-blue-900 text-white rounded-lg shadow">
+                    Explore DIY
+                    </button>
+                    <button className="px-6 py-3 border border-gray-400 rounded-lg">
+                    Learn More
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
